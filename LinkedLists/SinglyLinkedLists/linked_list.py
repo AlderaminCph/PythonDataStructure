2574fe3628119334da6709e7f432369c821ff920
Anastasia <sypkova.anastasia@yandex.ru>
2021-05-30 21:53:36 +0300

    summ of two lists method was added

--- LinkedLists/SinglyLinkedLists/linked_list.py
+++ LinkedLists/SinglyLinkedLists/linked_list.py
@@ -370,13 +370,55 @@ class LinkedList:
         second_to_the_last.next = None
         self.head = last
 
-llist = LinkedList()
-llist.append('R')
-llist.append('A')
-llist.append('D')
-llist.append('A')
-llist.append('R')
-llist.print_list()
-print('\n')
-llist.move_tail_to_head()
-llist.print_list()
+    def sum_two_lists(self,llist):
+        '''
+        3 6 5
+        2 4 8
+        -----
+        6 1 3
+        '''
+        p = self.head
+        q = llist.head
+
+        sum_list = LinkedList() # our final list
+        carry = 0
+
+        while p or q:
+            if not p:
+                i = 0
+            else:
+                i = p.data
+            if not q:
+                j = 0
+            else:
+                j = q.data
+            s = i + j + carry
+            if s >= 10:
+                carry = 1
+                remainder = s % 10
+                sum_list.append(remainder)
+            else:
+                carry = 0
+                sum_list.append(s)
+            if p:
+                p = p.next
+            if q:
+                q = q.next
+        sum_list.print_list()
+
+# 3 6 5 
+#   4 2 
+# ------
+#  
+llist1 = LinkedList()
+llist1.append(5)
+llist1.append(6)
+llist1.append(3)
+
+llist2 = LinkedList()
+llist2.append(8)
+llist2.append(4)
+llist2.append(2)
+
+print(365 + 248)
+llist1.sum_two_lists(llist2)
